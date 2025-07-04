4e7b582420f1d8aaf31f3ada596e14cb9cd5a804
0xE4F90A <96418802+0xE4F90A@users.noreply.github.com>
2025-07-04 12:11:42 +0900

    RInput Sample Project
    Visual Studio 2022

---
+++ RInputSampleProject/IncludeLib/RInput.h
@@ -0,0 +1,297 @@
+﻿/******************************************************************************
+ *  File: RInput.h
+ *
+ *  Helpers for RawInputAPI
+ *
+ *  Copyright (c) 0xE4F90A. All rights reserved.
+ *  Licensed under the MIT License.
+ *****************************************************************************/
+#ifndef RINPUT_INCLUDE_GUARD
+#define RINPUT_INCLUDE_GUARD
+//---------------------------------------
+#ifndef WIN32_LEAN_AND_MEAN
+#  define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#  define NOMINMAX
+#endif
+#include <Windows.h>
+#include <cstdint>
+
+#ifdef RINPUT_EXPORTS
+#  define RINPUT_API __declspec(dllexport)
+#else
+#  define RINPUT_API __declspec(dllimport)
+#endif
+
+namespace RInput
+{
+	//―――――――― KeyCode ――――――――
+	enum class RawKey : std::uint16_t
+	{
+		Unknown = 0,
+
+		// --- Control / Edit ---
+		Backspace	 = 0x08,
+		Tab			 = 0x09,
+		Clear		 = 0x0C,
+		Enter		 = 0x0D,
+		Shift		 = 0x10,
+		Control		 = 0x11,
+		Alt			 = 0x12,
+		Pause		 = 0x13,
+		CapsLock	 = 0x14,
+		Escape		 = 0x1B,
+		Space		 = 0x20,
+		PageUp		 = 0x21,
+		PageDown	 = 0x22,
+		End			 = 0x23,
+		Home		 = 0x24,
+		Left		 = 0x25,
+		Up			 = 0x26,
+		Right		 = 0x27,
+		Down		 = 0x28,
+		Select		 = 0x29,
+		Print		 = 0x2A,
+		Execute		 = 0x2B,
+		PrintScreen	 = 0x2C,
+		Insert		 = 0x2D,
+		DeleteKey	 = 0x2E,
+		Help		 = 0x2F,
+
+		// --- 0-9 ---
+		Num0 = '0',
+		Num1 = '1',
+		Num2 = '2',
+		Num3 = '3',
+		Num4 = '4',
+		Num5 = '5',
+		Num6 = '6',
+		Num7 = '7',
+		Num8 = '8',
+		Num9 = '9',
+
+		// --- A-Z ---
+		A = 'A',
+		B = 'B',
+		C = 'C',
+		D = 'D',
+		E = 'E',
+		F = 'F',
+		G = 'G',
+		H = 'H',
+		I = 'I',
+		J = 'J',
+		K = 'K',
+		L = 'L',
+		M = 'M',
+		N = 'N',
+		O = 'O',
+		P = 'P',
+		Q = 'Q',
+		R = 'R',
+		S = 'S',
+		T = 'T',
+		U = 'U',
+		V = 'V',
+		W = 'W',
+		X = 'X',
+		Y = 'Y',
+		Z = 'Z',
+
+		// --- Windows & Incorporated ---
+		LWin	 = 0x5B,
+		RWin	 = 0x5C,
+		Apps	 = 0x5D,
+		Sleep	 = 0x5F,
+
+		// --- NumPad ---
+		NumPad0		 = 0x60,
+		NumPad1		 = 0x61,
+		NumPad2		 = 0x62,
+		NumPad3		 = 0x63,
+		NumPad4		 = 0x64,
+		NumPad5		 = 0x65,
+		NumPad6		 = 0x66,
+		NumPad7		 = 0x67,
+		NumPad8		 = 0x68,
+		NumPad9		 = 0x69,
+		NumPadMul	 = 0x6A,
+		NumPadAdd	 = 0x6B,
+		NumPadSep	 = 0x6C,
+		NumPadSub	 = 0x6D,
+		NumPadDec	 = 0x6E,
+		NumPadDiv	 = 0x6F,
+
+		// --- F1-F24 ---
+		F1 = 0x70,
+		F2,
+		F3,
+		F4,
+		F5,
+		F6,
+		F7,
+		F8,
+		F9,
+		F10,
+		F11,
+		F12,
+		F13 = 0x7C,
+		F14,
+		F15,
+		F16,
+		F17,
+		F18,
+		F19,
+		F20,
+		F21,
+		F22,
+		F23,
+		F24,
+
+		// --- Lock ---
+		NumLock		 = 0x90,
+		ScrollLock	 = 0x91,
+
+		// --- Extended modifiers key---
+		LShift	 = 0xA0,
+		RShift	 = 0xA1,
+		LControl = 0xA2,
+		RControl = 0xA3,
+		LAlt	 = 0xA4,
+		RAlt	 = 0xA5,
+
+		// --- Browser / Media key ---
+		BrowserBack		 = 0xA6,
+		BrowserForward	 = 0xA7,
+		BrowserRefresh	 = 0xA8,
+		BrowserStop		 = 0xA9,
+		BrowserSearch	 = 0xAA,
+		BrowserFavorites = 0xAB,
+		BrowserHome		 = 0xAC,
+		VolumeMute		 = 0xAD,
+		VolumeDown		 = 0xAE,
+		VolumeUp		 = 0xAF,
+		MediaNextTrack	 = 0xB0,
+		MediaPrevTrack	 = 0xB1,
+		MediaStop		 = 0xB2,
+		MediaPlayPause	 = 0xB3,
+		LaunchMail		 = 0xB4,
+		LaunchMediaSel	 = 0xB5,
+		LaunchApp1		 = 0xB6,
+		LaunchApp2		 = 0xB7,
+
+		// --- OEM (US Keyboard) ---
+		OemSemicolon	 = 0xBA,	// ;:
+		OemPlus			 = 0xBB,	// =+
+		OemComma		 = 0xBC,	// ,<
+		OemMinus		 = 0xBD,	// -_
+		OemPeriod		 = 0xBE,	// .>
+		OemSlash		 = 0xBF,	// /?
+		OemTilde		 = 0xC0,	// `~
+		OemOpenBracket	 = 0xDB,	// [{
+		OemBackslash	 = 0xDC,	// \|
+		OemCloseBracket	 = 0xDD,	// ]}
+		OemQuote		 = 0xDE,	// '"
+		Oem8			 = 0xDF,
+		Oem102			 = 0xE2,	// "<>" or "\|"
+
+		// --- IME / etc ---
+		ProcessKey		 = 0xE5,
+		ImeKana			 = 0x15,	// Kana / Kanji
+		ImeJunja		 = 0x17,
+		ImeFinal		 = 0x18,
+		ImeConvert		 = 0x1C,
+		ImeNonConvert	 = 0x1D,
+		ImeAccept		 = 0x1E,
+		ImeModeChange	 = 0x1F,
+	};
+
+	enum class MouseButton : std::uint8_t
+	{
+		Left = 0,
+		Right,
+		Middle,
+		X1,
+		X2
+	};
+
+	struct Vec2
+	{
+		int x;
+		int y;
+	};
+
+	struct Context;	// forward
+
+	// Callback
+	using KeyCallback = void(*)(RawKey);
+	using MouseBtnCallback = void(*)(MouseButton);
+	using MouseMoveCallback = void(*)(int dx, int dy);
+	using MouseWheelCallback = void(*)(int delta);
+
+	struct DeviceInfo
+	{
+		wchar_t       name[128];
+		std::uint16_t vendorID;
+		std::uint16_t productID;
+	};
+
+}
+// namespace RInput
+
+extern "C"
+{
+	//―― Base ――
+	RINPUT_API RInput::Context* CreateContext(HWND, std::uint32_t flags = 0);
+	RINPUT_API void             DestroyContext(RInput::Context*);
+	RINPUT_API void             OnMessage(RInput::Context*, HWND, UINT, WPARAM, LPARAM);
+	RINPUT_API void             Update(RInput::Context*);
+
+	//― keyboard ―
+	RINPUT_API bool	GetKeyDown(RInput::Context*, RInput::RawKey);
+	RINPUT_API bool	GetKey(RInput::Context*, RInput::RawKey);
+	RINPUT_API bool	GetKeyUp(RInput::Context*, RInput::RawKey);
+
+	//― ScanCode ―
+	RINPUT_API bool	GetScanCodeDown(RInput::Context*, std::uint16_t);
+	RINPUT_API bool	GetScanCode(RInput::Context*, std::uint16_t);
+	RINPUT_API bool	GetScanCodeUp(RInput::Context*, std::uint16_t);
+
+	//― Mouse ―
+	RINPUT_API bool			GetMouseButtonDown(RInput::Context*, RInput::MouseButton);
+	RINPUT_API bool			GetMouseButton(RInput::Context*, RInput::MouseButton);
+	RINPUT_API bool			GetMouseButtonUp(RInput::Context*, RInput::MouseButton);
+	RINPUT_API RInput::Vec2	GetMousePosition(RInput::Context*);
+	RINPUT_API RInput::Vec2	GetMouseDelta(RInput::Context*);
+	RINPUT_API int			GetMouseWheel(RInput::Context*);
+
+	//― TextInput ―
+	RINPUT_API void	EnableTextInput(RInput::Context*, bool);
+	RINPUT_API void	OnChar(RInput::Context*, wchar_t);
+	RINPUT_API void	OnImeChar(RInput::Context*, wchar_t);
+	RINPUT_API int	GetCharInput(RInput::Context*, std::uint32_t* out, int max);
+	RINPUT_API int	GetCommittedString(RInput::Context*, wchar_t* out, int max);
+
+	//― Cursor ―
+	RINPUT_API void	SetCursorVisible(RInput::Context*, bool);
+	RINPUT_API void	ClipCursorToWindow(RInput::Context*, bool);
+	RINPUT_API void	WarpCursor(RInput::Context*, int, int);
+
+	//― Callback ―
+	RINPUT_API void	RegisterKeyDownCallback(RInput::Context*, RInput::KeyCallback);
+	RINPUT_API void	RegisterKeyUpCallback(RInput::Context*, RInput::KeyCallback);
+	RINPUT_API void	RegisterMouseDownCallback(RInput::Context*, RInput::MouseBtnCallback);
+	RINPUT_API void	RegisterMouseUpCallback(RInput::Context*, RInput::MouseBtnCallback);
+	RINPUT_API void	RegisterMouseMoveCallback(RInput::Context*, RInput::MouseMoveCallback);
+	RINPUT_API void	RegisterMouseWheelCallback(RInput::Context*, RInput::MouseWheelCallback);
+
+	//― Device list ―
+	RINPUT_API int GetRegisteredDevice(RInput::Context*, RInput::DeviceInfo*, int);
+
+	//― Utility ―
+	RINPUT_API bool	AnyKeyDown(RInput::Context*);
+	RINPUT_API void	ResetInput(RInput::Context*);
+}
+// extern "C"
+#endif
---
+++ RInputSampleProject/WinMain.cpp
@@ -0,0 +1,276 @@
+﻿// RInputSample - All API demo
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#include <tchar.h>
+#include <fcntl.h>
+#include <io.h>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+
+// RInput Header `$(SolutionDir)/IncludeLib`
+#include <RInput.h>
+
+#pragma comment(lib,"user32.lib")
+#pragma comment(lib,"gdi32.lib")
+
+// RInput Library `$(SolutionDir)/IncludeLib`
+#pragma comment(lib,"RInput.lib")
+
+// Globals
+static RInput::Context* g_ctx = nullptr;
+static std::wstring     g_textLine;             // WM_CHAR
+static int              g_wheelAcc = 0;         // Wheel data
+static bool             g_showCursor = true;
+static bool             g_clipCursor = false;
+static POINT            g_lastDelta{ 0,0 };     // GetMouseDelta result
+static bool             g_anyKey = false;       // AnyKeyDown result
+
+constexpr int WIDTH = 500;
+constexpr int HEIGHT = 500;
+
+
+// Utilities
+static void Log(const std::wstring& s)
+{
+	OutputDebugStringW((s + L"\n").c_str());
+	std::wcout << s.c_str();
+}
+
+// Callbacks
+void OnKeyDown(RInput::RawKey k)
+{
+	if (k == RInput::RawKey::F2)
+	{
+		g_showCursor = !g_showCursor;
+		SetCursorVisible(g_ctx, g_showCursor);
+	}
+	else if (k == RInput::RawKey::F3)
+	{
+		g_clipCursor = !g_clipCursor;
+		ClipCursorToWindow(g_ctx, g_clipCursor);
+	}
+	else if (k == RInput::RawKey::F4)
+	{
+		RECT rc{};
+		GetClientRect(WindowFromDC(GetDC(NULL)), &rc);
+		WarpCursor(g_ctx, WIDTH / 2, HEIGHT / 2);
+	}
+	else if (k == RInput::RawKey::F6)
+	{
+		ResetInput(g_ctx);
+		g_textLine.clear();
+		g_wheelAcc = 0;
+	}
+
+	std::wstringstream ss{};
+	ss << L"[KeyDown] " << (std::uint16_t)k;
+	Log(ss.str());
+}
+void OnKeyUp(RInput::RawKey k)
+{
+	std::wstringstream ss{};
+	ss << L"[KeyUp] " << (std::uint16_t)k;
+	Log(ss.str());
+}
+
+void OnMouseDown(RInput::MouseButton b)
+{
+	std::wstringstream ss{};
+	ss << L"[MouseDown] " << (int)b;
+	Log(ss.str());
+}
+
+void OnMouseUp(RInput::MouseButton b)
+{
+	std::wstringstream ss{};
+	ss << L"[MouseUp] " << (int)b;
+	Log(ss.str());
+}
+
+void OnMouseMove(int dx, int dy)
+{
+	g_lastDelta.x = dx;
+	g_lastDelta.y = dy;
+}
+
+void OnWheel(int d)
+{
+	g_wheelAcc += d;
+}
+
+// Draw helper
+static void DrawLine(HDC h, int& y, const wchar_t* fmt, ...)
+{
+	wchar_t buf[512]{};
+	va_list vl{};
+	va_start(vl, fmt);
+	_vsnwprintf_s(buf, 512, fmt, vl);
+	va_end(vl);
+	TextOutW(h, 10, y, buf, (int)wcslen(buf));
+	y += 18;
+}
+
+static void DrawWrappedText(HDC hdc, int& y, const std::wstring& str, int maxWidthPx, int lineH = 18, int marginX = 10)
+{
+	std::size_t head = 0;
+
+	while (head < str.size())
+	{
+		std::size_t len = 1;
+
+		for (; head + len <= str.size(); ++len)
+		{
+			SIZE sz{};
+			GetTextExtentPoint32W(hdc, &str[head], (int)len, &sz);
+
+			if (sz.cx > maxWidthPx - marginX * 2)
+			{
+				--len;
+				if (len == 0) len = 1;
+				break;
+			}
+		}
+		TextOutW(hdc, marginX, y, &str[head], (int)len);
+		y += lineH;
+		head += len;
+	}
+}
+
+// Window Proc
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	switch (msg)
+	{
+	case WM_CREATE:
+	{
+		g_ctx = CreateContext(hWnd, 0);
+
+		RegisterKeyDownCallback(g_ctx, OnKeyDown);
+		RegisterKeyUpCallback(g_ctx, OnKeyUp);
+		RegisterMouseDownCallback(g_ctx, OnMouseDown);
+		RegisterMouseUpCallback(g_ctx, OnMouseUp);
+		RegisterMouseMoveCallback(g_ctx, OnMouseMove);
+		RegisterMouseWheelCallback(g_ctx, OnWheel);
+		EnableTextInput(g_ctx, true);
+
+		// Show Device List
+		RInput::DeviceInfo infos[32]{};
+		int n = GetRegisteredDevice(g_ctx, infos, 32);
+
+		for (int i = 0; i < n; ++i)
+		{
+			std::wstringstream ss{};
+			ss << L"[Device] " << infos[i].name
+				<< L" VID:" << std::hex << infos[i].vendorID
+				<< L" PID:" << infos[i].productID;
+			Log(ss.str());
+		}
+
+		return 0;
+	}
+
+	case WM_INPUT:
+	{
+		OnMessage(g_ctx, hWnd, msg, wp, lp);
+		return 0;
+	}
+	case WM_CHAR:
+	{
+		OnChar(g_ctx, (wchar_t)wp);
+		g_textLine.push_back((wchar_t)wp);
+		return 0;
+	}
+	case WM_IME_CHAR:
+	{
+		OnImeChar(g_ctx, (wchar_t)wp);
+		return 0;
+	}
+	case WM_TIMER:
+	{
+		Update(g_ctx);
+		g_anyKey = AnyKeyDown(g_ctx);
+		InvalidateRect(hWnd, nullptr, FALSE);
+		return 0;
+	}
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps{};
+		HDC hdc = BeginPaint(hWnd, &ps);
+		RECT rc{};
+		GetClientRect(hWnd, &rc);
+		FillRect(hdc, &rc, (HBRUSH)(COLOR_WINDOW + 1));   // clear
+
+		int y = 10; SetBkMode(hdc, TRANSPARENT);
+
+		// Keyboard
+		DrawLine(hdc, y, L"===== Keyboard =====");
+		DrawLine(hdc, y, L"AnyKeyDown: %d", g_anyKey);
+
+		for (char c = 'A'; c <= 'Z'; ++c)
+			if (GetKey(g_ctx, (RInput::RawKey)c))
+				DrawLine(hdc, y, L"%c Hold", c);
+
+		if (GetKey(g_ctx, RInput::RawKey::Space))
+			DrawLine(hdc, y, L"Space Hold");
+
+		// Mouse
+		auto pos = GetMousePosition(g_ctx);
+		DrawLine(hdc, y, L"===== Mouse =====");
+		DrawLine(hdc, y, L"Pos: [%d, %d]    Delta: [%d, %d]", pos.x, pos.y, g_lastDelta.x, g_lastDelta.y);
+		DrawLine(hdc, y, L"Buttons: [L: %d,   R: %d,   M: %d,   B: %d,   F: %d]",
+			GetMouseButton(g_ctx, RInput::MouseButton::Left),
+			GetMouseButton(g_ctx, RInput::MouseButton::Right),
+			GetMouseButton(g_ctx, RInput::MouseButton::Middle),
+			GetMouseButton(g_ctx, RInput::MouseButton::X1),
+			GetMouseButton(g_ctx, RInput::MouseButton::X2));
+		DrawLine(hdc, y, L"Cursor: %d,   Clip: %d", g_showCursor, g_clipCursor);
+		DrawLine(hdc, y, L"Wheel total: %d (F2: Cursor,   F3: Clip,   F4: Warp,   F6: Reset)", g_wheelAcc);
+
+		// Text
+		DrawLine(hdc, y, L"===== TextInput =====");
+		GetClientRect(hWnd, &rc);
+		DrawWrappedText(hdc, y, g_textLine, rc.right - rc.left);
+
+		EndPaint(hWnd, &ps);
+		return 0;
+	}
+
+	case WM_DESTROY:
+		DestroyContext(g_ctx);
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, msg, wp, lp);
+}
+
+// wWinMain
+int __stdcall wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPTSTR, _In_ int)
+{
+	WNDCLASSEX wc
+	{
+		sizeof(wc), CS_DBLCLKS, WndProc, 0, 0, hInstance, nullptr, LoadCursor(nullptr, IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1), nullptr, _T("RInputWnd"), nullptr
+	};
+
+	RegisterClassEx(&wc);
+
+	HWND hWnd = CreateWindow(wc.lpszClassName, _T("RInput Library Test App"),
+		WS_OVERLAPPEDWINDOW, 200, 200, WIDTH, HEIGHT, nullptr, nullptr, hInstance, nullptr);
+
+	SetTimer(hWnd, 1, 33, nullptr);	 // 30 fps
+
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+
+	MSG msg{};
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
